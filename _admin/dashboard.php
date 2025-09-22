88f4a0552e54312190b9ab5da4ca280e86b1df75
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-09-22 15:50:31 +0530

    Updates

--- _admin/dashboard.php
+++ _admin/dashboard.php
@@ -8,11 +8,32 @@ if (!is_loggedin()) {
 $document->setTitle(trans('title_dashboard'));
 $document->setController('DashboardController');
 include('src/_top.php');
+
+$total_products = get_count('product', null, null, 0);
+$gold_products = get_count('product', 'material', 2, 0);
+$silver_products = get_count('product', 'material', 8, 0);
+$customers = get_count('customer');
+$orders = get_count('orders');
+$unfinished_orders = get_count('orders', 'order_status', 0);
+$invoices = get_count('invoice_info', null, null, 0);
+$sold = get_count('product', null, null, 3);
+$sold = get_count('product', null, null, 3);
+$today_sold = get_count('invoice_info', 'created_at', null, null, true);
+$today_pre_order = get_count('orders', 'order_status', 0, null, true);
+
+$total_golds_weight = get_sum('product', 'wgt', 'material', 2, 0);
+$total_silver_weight = get_sum('product', 'wgt', 'material', 8, 0);
+$unfinished_orders_amount = get_sum('orders', 'total_amt', 'order_status', 0);
+$unfinished_orders_total_paid = get_sum('orders', 'total_paid', 'order_status', 0);
+$unfinished_orders_outstanding = $unfinished_orders_amount - $unfinished_orders_total_paid;
+$invoice_outstanding = get_sum('invoice_info', 'outstanding', null, null, 0);
+$today_sales_amount = get_sum('invoice_info', 'total_payable', null, null, 0, true);
+$today_orders_amount = get_sum('orders', 'total_amt', 'order_status', 0, null, true);
 ?>
 
 <div class="wrapper">
     <div class="content-header">
-        <div class="container-fluid">
+        <div class="container-fluid p-0">
             <div class="row mb-2 card p-2 d-flex align-items-center justify-content-between">
                 <span class="font-weight-bold"
                     style="font-size: 1.3rem; color: rgb(140, 140, 140); font-weight: 500;">
@@ -24,6 +45,88 @@ include('src/_top.php');
         </div>
     </div>
 
+    <div class="card-body row p-1">
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_total_pieces") ?></span><span class="badge bg-warning"><?php echo $total_products ?></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_gold_pieces") ?></span><span><span class="badge bg-warning"><?php echo $gold_products ?></span> <span class="badge bg-warning"><?php echo $total_golds_weight . 'g' ?></span></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_silver_pieces") ?></span><span><span class="badge bg-warning"><?php echo $silver_products ?></span> <span class="badge bg-warning"><?php echo $total_silver_weight . 'g' ?></span></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_customers") ?></span><span class="badge bg-warning"><?php echo $customers ?></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_total_pre_orders") ?></span><span class="badge bg-warning"><?php echo $orders ?></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_unfinished_pre_orders") ?></span><span><span class="badge bg-warning"><?php echo $unfinished_orders ?></span> <span class="badge bg-warning"><?php echo 'Rs.' . number_format($unfinished_orders_outstanding, 2) ?></span></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_total_invoices") ?></span><span class="badge bg-warning"><?php echo $invoices ?></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_invoice_outstandings") ?></span><span><span class="badge bg-warning"><?php echo $invoices ?></span> <span class="badge bg-warning"><?php echo 'Rs.' . number_format($invoice_outstanding, 2) ?></span></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_sold_pieces") ?></span><span class="badge bg-warning"><?php echo $sold ?></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_today's_sale") ?></span><span><span class="badge bg-warning"><?php echo $today_sold ?></span> <span class="badge bg-warning"><?php echo 'Rs.' . number_format($today_sales_amount, 2) ?></span></span></p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2 px-1 pb-2">
+            <div class="col-12 border border-primary rounded py-2 px-3">
+                <p class="font-weight-bold m-0 row justify-content-between"><span><?php echo trans("text_today's_pre_orders") ?></span><span><span class="badge bg-warning"><?php echo $today_sold ?></span> <span class="badge bg-warning"><?php echo 'Rs.' . number_format($today_orders_amount, 2) ?></span></p>
+            </div>
+        </div>
+    </div>
+
+    <form ng-submit="updateMaterialPrice()" id="material-price-update" class="card card-outline card-primary mt-2">
+        <div class="card-header py-2 px-3">
+            <h6><?php echo trans("label_today_materials_price") . ' (' . date("d F, Y") . ')' ?></h6>
+        </div>
+        <div class="row p-4">
+            <div class="form-group col-12 col-sm-6 col-xl-3 py-2 pr-2 m-0">
+                <label>22k Gold <span class="text-danger">*</span></label>
+                <input type="text" ng-model="materials.gold22" class="form-control" placeholder="22k Gold Price">
+            </div>
+            <div class="form-group col-12 col-sm-6 col-xl-3 py-2 px-2 m-0">
+                <label>24k Gold <span class="text-danger">*</span></label>
+                <input type="text" ng-model="materials.gold24" class="form-control" placeholder="24k Gold Price">
+            </div>
+            <div class="form-group col-12 col-sm-6 col-xl-3 py-2 px-2 m-0">
+                <label>Silver <span class="text-danger">*</span></label>
+                <input type="text" ng-model="materials.silver" class="form-control" placeholder="Silver Price">
+            </div>
+            <div class="form-group col-12 col-sm-6 col-xl-3 pl-2 m-0 pt-2 mt-1">
+                <button type="submit" class="btn btn-success w-100 mt-4">
+                    <i class="fas fa-save"></i> Update Price
+                </button>
+            </div>
+        </div>
+    </form>
 
 </div>
 <?php
--- assets/nit/js/angular/controller/DashboardController.js
+++ assets/nit/js/angular/controller/DashboardController.js
@@ -1,3 +1,103 @@
-angularApp.controller("DashboardController", ["$scope", "API_URL", "window", "jQuery", "$compile", "$uibModal", "$http", "$sce", function ($scope, API_URL, window, $, $compile, $uibModal, $http, $sce) {
-    $scope.title = "welcome";
+angularApp.controller("DashboardController", ["$scope", "$http", "API_URL", function ($scope, $http, API_URL) {
+
+    $http({
+        method: 'GET',
+        url: window.baseUrl + '/_inc/_material_price.php',
+        params: { action_type: "GET_MATERIAL_PRICES" },
+        responseType: "json"
+    }).then(
+        function (response) {
+            $scope.materials = response.data.data;
+            $scope.lastMaterialPrices = angular.copy($scope.materials);
+        },
+        function (response) {
+            var alertMsg = response.data && typeof response.data === 'object'
+                ? Object.values(response.data).join(" ")
+                : response.statusText || "Error fetching material prices";
+            Toast.fire({ icon: 'error', title: 'Oops!', text: alertMsg });
+        }
+    )
+    // Function to send changed prices to backend
+    function sendMaterialPrices(formData) {
+        $http({
+            method: 'POST',
+            url: window.baseUrl + '/_inc/_material_price.php?action_type=CREATE',
+            data: formData,
+            cache: false,
+            processData: false,
+            contentType: false,
+            dataType: "json"
+        }).then(function (response) {
+            Swal.fire({
+                title: "Updated!",
+                icon: "success",
+                text: 'Material prices updated successfully.'
+            });
+            $scope.materials = response.data.data;
+            $scope.lastMaterialPrices = angular.copy($scope.materials);
+        }, function (error) {
+            Toast.fire({ icon: 'error', title: 'Error!', text: 'Failed to update prices.' });
+        });
+    }
+
+    $scope.updateMaterialPrice = function () {
+        // Validate inputs individually
+        if (!$scope.materials.gold22) return Toast.fire({ icon: 'error', title: 'Error!', text: 'Please enter 22k gold price' });
+        if (!$scope.materials.gold24) return Toast.fire({ icon: 'error', title: 'Error!', text: 'Please enter 24k gold price' });
+        if (!$scope.materials.silver) return Toast.fire({ icon: 'error', title: 'Error!', text: 'Please enter silver price' });
+
+        // Prepare only changed prices
+        let formData = {};
+        let warnings = [];
+
+        if ($scope.materials.gold22 !== $scope.lastMaterialPrices.gold22) {
+            formData.gold22 = $scope.materials.gold22;
+        } else {
+            warnings.push("22k Gold");
+        }
+
+        if ($scope.materials.gold24 !== $scope.lastMaterialPrices.gold24) {
+            formData.gold24 = $scope.materials.gold24;
+        } else {
+            warnings.push("24k Gold");
+        }
+
+        if ($scope.materials.silver !== $scope.lastMaterialPrices.silver) {
+            formData.silver = $scope.materials.silver;
+        } else {
+            warnings.push("Silver");
+        }
+
+        // If all prices are unchanged, alert and stop
+        if (Object.keys(formData).length === 0) {
+            return Swal.fire("All entered prices are same as the last updated values. Nothing to update.");
+        }
+        console.log(warnings.length)
+
+        // If some prices are unchanged, alert which ones will not be updated
+        if (warnings.length > 0) {
+            Swal.fire({
+                title: "Note",
+                text: warnings.join(", ") + ` ${warnings.length > 1 ? 'prices are' : 'price is'} same as last updated. Only changed prices will be updated.`,
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#218838",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Update!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Serialize only changed prices
+                    let serializedData = $.param(formData).replace(/%2C/g, '').replace(/\.00/g, '');
+                    // Send to backend
+                    sendMaterialPrices(serializedData);
+                }
+            })
+        } else {
+            // Serialize only changed prices
+            let serializedData = $.param(formData).replace(/%2C/g, '').replace(/\.00/g, '');
+            // Send to backend
+            sendMaterialPrices(serializedData);
+        }
+
+    }
 }]);
